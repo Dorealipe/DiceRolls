0fa761cdff3df7f9214adc09a0f977bc74bfc37c
Dorealipe <doreafelipe12@gmail.com>
2025-12-29 14:26:57 +0000

    Fixed dice expressions not working

--- interp/dice.py
+++ interp/dice.py
@@ -5,5 +5,10 @@ class Die:
     def play(self):
        return Die.random.choice(self.sides)
 class FairDie(Die):
-    def __init__(self,quant_sides:int):
-        super().__init__(list(range(quant_sides)))
+    def __init__(self,quant_sides:int|str):
+        if isinstance(quant_sides,int):
+            super().__init__(list(range(quant_sides)))
+        elif isinstance(quant_sides,str):
+            if quant_sides[0] == 'd' and  quant_sides[1:].isalpha():
+                super().__init__(list(range(int(quant_sides[1:]))))
+            else: raise TypeError('Strings must have the format dX, where X is a natural number')
