362439e0dfbe57034d053e3fe49b36909856581e
Dorealipe <doreafelipe12@gmail.com>
2025-12-29 11:42:26 -0300

    Implement __str__ methods for Die and fixed errors
    Added string representation methods for Die and FairDie classes and changed isalpha to  isdigit

--- interp/dice.py
+++ interp/dice.py
@@ -4,11 +4,16 @@ class Die:
         self.sides = sides
     def play(self):
        return Die.random.choice(self.sides)
+    def __str__(self):
+        return f'd{min(self.sides)}~{max(self.sides)}'
 class FairDie(Die):
     def __init__(self,quant_sides:int|str):
         if isinstance(quant_sides,int):
             super().__init__(list(range(quant_sides)))
+            
         elif isinstance(quant_sides,str):
-            if quant_sides[0] == 'd' and  quant_sides[1:].isalpha():
+            if quant_sides[0] == 'd' and  quant_sides[1:].isdigit():
                 super().__init__(list(range(int(quant_sides[1:]))))
             else: raise TypeError('Strings must have the format dX, where X is a natural number')
+    def __str__(self):
+        return f'd{max(self.sides)}'
