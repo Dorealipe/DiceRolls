949c3ae696e9fd481e390ee33db72c5d2d0948ad
Dorealipe <doreafelipe12@gmail.com>
2026-01-12 17:32:10 -0300

    Add Python version check and installation of colorama in setup script

--- setup.ps1
+++ setup.ps1
@@ -1,8 +1,33 @@
+
+
+
 # Checks if has admin permission 
 if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     $arguments = "& '" + $myinvocation.mycommand.definition + "'"
     Start-Process powershell -Verb runAs -ArgumentList $arguments
     break
+}
+#Checks if python or python3 is usable
+try {
+    python --version 2>$null
+    if ($LASTEXITCODE -eq 0){
+    $PyPath = "python"
+    }
+}
+catch {
+    try {
+        python3 --version 2>$null
+        if ($LASTEXITCODE -eq 0){
+            $PyPath = "python3"
+        }
+    }
+    catch{
+    throw "Error: Python must be installed"
+    $PyPath = "None"
+}
+}
+if ($PyPath -notlike "None") {
+    Invoke-Expression "$PyPath -m pip install colorama" # python -m pip install colorama
 }
 
 # Gets path
@@ -18,5 +43,6 @@ if ($CurrentPath -notlike "*$RepoPath\interp*") {
 } else {
     Write-Host "dr already in PATH." -ForegroundColor Cyan
 }
+
 
 pause
